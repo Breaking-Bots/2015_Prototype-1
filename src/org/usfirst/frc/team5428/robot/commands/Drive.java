e371cb99c7205e123f80075523c59f6865c70720
Zaeem Mohamed <zaeemkmohamed@gmail.com>
2015-01-10 22:16:41 -0500

    Single Drive Command that can alternateon the fly

--- src/org/usfirst/frc/team5428/robot/commands/Drive.java
+++ src/org/usfirst/frc/team5428/robot/commands/Drive.java
@@ -2,17 +2,25 @@ package org.usfirst.frc.team5428.robot.commands;
 
 import org.usfirst.frc.team5428.robot.core.CommandBase;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * Drive via arcade drive
+ * an Drive class, for on the fly drive switching
  */
-public class ArcadeDrive extends CommandBase {
+public class Drive extends CommandBase {
 
 	private final float defaultMagnitude;
 	private float magnitude;
 	
-    public ArcadeDrive(float magnitude) {
+	public static final int TNK = 0;
+	public static final int ARC = 1;
+	public static final int ALT = 2;
+	
+	private int currentState = TNK;
+	
+    public Drive(float magnitude) {
     	requires(driveTrain);
         this.magnitude = magnitude;
         defaultMagnitude = magnitude;
@@ -24,22 +32,34 @@ public class ArcadeDrive extends CommandBase {
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	driveTrain.arcadeDrive(oi.driverController, magnitude);
+    protected void execute(){
+    	switch(currentState){
+    	case TNK:
+    		driveTrain.tankDrive(oi.driverController, magnitude);
+    		break;
+    	case ARC:
+    		driveTrain.arcadeDrive(oi.driverController, magnitude);
+    		break;
+    	case ALT:
+    		driveTrain.altDrive(oi.driverController, magnitude);
+    		break;
+    	default:
+    		SmartDashboard.putString("Error", "Invalid Drive State");	    			
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    protected final boolean isFinished() {
         return false;
     }
 
     // Called once after isFinished returns true
-    protected void end() {
+    protected final void end() {
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() {
+    protected final void interrupted() {
     }
     
     public float getMagnitude() {
@@ -52,6 +72,14 @@ public class ArcadeDrive extends CommandBase {
 	
 	public void defaultMagnitude(){
 		this.magnitude = defaultMagnitude;
+	}
+
+	public int getCurrentState() {
+		return currentState;
+	}
+
+	public void setCurrentState(int currentState) {
+		this.currentState = currentState;
 	}
     
 }
